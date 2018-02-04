bcbf596fa1a5ed3f4f6428fcd83a1d213ea93ba2
sylvain <sylvainmaestri@gmail.com>
2018-02-04 13:44:59 +0100

    first commit

--- app/myComponent.ts
+++ app/myComponent.ts
@@ -1,8 +1,8 @@
-// import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
-// @Component({
-//     selector: 'app',
-//     template: '<div>HELLO</div>',
-// })
-// export class AppComponent {
-// }
+@Component({
+    selector: 'app',
+    template: '<div>Hello World</div>',
+})
+export class AppComponent {
+}
